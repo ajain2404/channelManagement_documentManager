6dfbf5fff436a87b6714ffd99024e2fb7e360167
Shabeer Khan <shabeer_skhan@infosys.com>
2018-09-21 02:40:32 +0000

    Import content for version 1.0.5

---
+++ Component.js
@@ -0,0 +1,40 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+	"com/evonik/appsDocumentManager/model/models"
+], function(UIComponent, Device, models) {
+	"use strict";
+
+	return UIComponent.extend("com.evonik.appsDocumentManager.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function() {
+			// call the base component's init function
+			var oStartupParameters = undefined;
+				
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+			
+			//get the document service repository name in query parameter
+			if(this.getComponentData()){
+				oStartupParameters = this.getComponentData( ).startupParameters;
+				if(oStartupParameters.repoName.length){
+					this.repositoryName = oStartupParameters.repoName[0];
+				}else{
+					//sap.m.MessageToast.show("i18n>noRepos", [mOptions])
+				}
+			}
+			console.log(this.getComponentData( ).startupParameters);
+		}
+	});
+});
