bb33692044553d215827ac36a63a71244bb5d652
V4719 <venkatakalyan.karanam@dgextern.com>
2020-01-13 14:38:50 +0000

    13.01.2020 Kalyan >> Latest code of Doc Manager

---
+++ controller/BaseController.js
@@ -0,0 +1,89 @@
+/*global history */
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
+], function(Controller, History) {
+	"use strict";
+
+	return Controller.extend("com.evonik.aminochick.apps.scenario.create.controller.BaseController", {
+		/**
+		 * Convenience method for accessing the router in every controller of the application.
+		 * @public
+		 * @returns {sap.ui.core.routing.Router} the router for this component
+		 */
+		getRouter: function() {
+			return this.getOwnerComponent().getRouter();
+		},
+
+		/**
+		 * Convenience method for getting the view model by name in every controller of the application.
+		 * @public
+		 * @param {string} sName the model name
+		 * @returns {sap.ui.model.Model} the model instance
+		 */
+		getModel: function(sName) {
+			return this.getView().getModel(sName);
+		},
+
+		/**
+		 * Convenience method for setting the view model in every controller of the application.
+		 * @public
+		 * @param {sap.ui.model.Model} oModel the model instance
+		 * @param {string} sName the model name
+		 * @returns {sap.ui.mvc.View} the view instance
+		 */
+		setModel: function(oModel, sName) {
+			return this.getView().setModel(oModel, sName);
+		},
+
+		/**
+		 * Convenience method for getting the resource bundle.
+		 * @public
+		 * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
+		 */
+		getResourceBundle: function() {
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+		},
+
+		/**
+		 * Event handler for navigating back.
+		 * It there is a history entry or an previous app-to-app navigation we go one step back in the browser history
+		 * If not, it will replace the current entry of the browser history with the master route.
+		 * @public
+		 */
+		onNavBack: function() {
+			var sPreviousHash = History.getInstance().getPreviousHash(),
+				oCrossAppNavigator = sap.ushell.Container.getService("CrossApplicationNavigation");
+
+			if (sPreviousHash !== undefined || !oCrossAppNavigator.isInitialNavigation()) {
+				history.go(-1);
+			} else {
+				this.getRouter().navTo("master", {}, true);
+			}
+		},
+
+		/**
+		 * 
+		 * Convenience method for getting the document service repository name which was 
+		 * passed to component.
+		 * @public
+		 * @returns {String} repositoryName the name of repository which was passed as URL parameter
+		 */
+		getRepositoryName: function() {
+			var oStartupParameters = null;
+			if (!this.repositoryName) {
+				if (this.getOwnerComponent().getComponentData()) {
+					oStartupParameters = this.getOwnerComponent().getComponentData().startupParameters;
+					if (oStartupParameters.repoName.length) {
+						this.repositoryName = oStartupParameters.repoName[0];
+					} else {
+						//sap.m.MessageToast.show("i18n>noRepos", [mOptions])
+					}
+				}
+			}
+			return this.repositoryName;
+		}
+
+	});
+
+});
